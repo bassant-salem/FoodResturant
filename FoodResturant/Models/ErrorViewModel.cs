b6d491de26b9f7ba017e2363cf084cc9c765c1c7
Bassant Salem <lyleee276@gmail.com>
2025-07-15 00:07:49 +0300

    Add project files.

---
+++ FoodResturant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodResturant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
